2fffd9ae01b7ec8ad19d97c7b0af3873913bc652
ChengHsuanLiu <chenghsuango@gmail.com>
2016-09-24 01:12:35 +0800

    add testing job

---
+++ app/jobs/create_posts_job.rb
@@ -0,0 +1,13 @@
+class CreatePostsJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+    puts 'hehehehehehe'
+    puts 'hehehehehehe'
+    puts 'hehehehehehe'
+    puts 'hehehehehehe'
+    puts 'hehehehehehe'
+    puts 'hehehehehehe'
+  end
+end
