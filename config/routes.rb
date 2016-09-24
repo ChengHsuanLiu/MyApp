f4b7794baa1775e3c214eb6bc846d847b31be270
ChengHsuanLiu <chenghsuango@gmail.com>
2016-09-24 20:48:42 +0800

    setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+  mount ActionCable.server => '/cable'
+
   devise_for :users
   get 'rooms/show'
-
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  require 'sidekiq/web'
-  mount Sidekiq::Web => '/sidekiq'
-
   root 'rooms#show'
-  mount ActionCable.server => '/cable'
 end
